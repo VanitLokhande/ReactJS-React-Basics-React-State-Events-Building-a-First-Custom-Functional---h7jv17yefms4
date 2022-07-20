577be19568bf87b492444d24b93200c219c4a92f
VanitLokhande <101892131+VanitLokhande@users.noreply.github.com>
2022-07-20 23:07:25 +0530

    assi

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
+import React, { useState } from "react";
+import "../styles/App.css";
+import Welcome from "./Welcome";
 
 const App = () => {
-  return (
-    <Welcome name="" />
-  )
-}
+  const [name, setname] = useState("");
 
+  const handlename = (event) => {
+    setname(event.target.value);
+  };
+  return (
+    <>
+      <input typt="text" onBlur={handlename} />
+      <Welcome name={name} />
+    </>
+  );
+};
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+
+const Welcome = (props) => {
+  return (
+    <>
+      <h1>Hey !{props.name}</h1>
+      <h1>Welcome to Newton School</h1>
+    </>
+  );
+};
+
+export default Welcome;
