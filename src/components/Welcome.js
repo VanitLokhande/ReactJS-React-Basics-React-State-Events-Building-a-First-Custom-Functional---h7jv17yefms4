5ad51e2a5030ecb55837d271875f30f05e5a458a
VanitLokhande <101892131+VanitLokhande@users.noreply.github.com>
2022-07-20 23:28:08 +0530

    a

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,7 @@ import React from "react";
 const Welcome = (props) => {
   return (
     <>
+      Greeting new students at Newton School.
       <h1>Hey !{props.name}</h1>
       <h2>Welcome to Newton School</h2>
     </>
